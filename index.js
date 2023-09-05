d7bfcdb26f11e41433bd7e23482dd4e605c58c7a
= <=>
2023-09-05 17:35:48 +0300

    server and db setup

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db';
 import router from './src/routes';
 
+dotenv.config();
+
+connectDB();
+
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 const PORT = process.env.PORT || 3000;
 
