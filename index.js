117c62c034926ce74146f88e8488f1a5423de66f
= <=>
2023-09-07 16:24:03 +0300

    middleware added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db';
 import router from './src/routes';
+import { errorHandler, notFound } from './src/middlewares/error';
 
 dotenv.config();
 
@@ -10,11 +11,14 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3000;
 
 app.use('/api', router);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log('Running on', PORT);
