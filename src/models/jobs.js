891a136123c079bd7d6387475567f2618081114c
= <=>
2023-09-07 16:23:44 +0300

    schema added

---
+++ src/models/jobs.js
@@ -0,0 +1,69 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const jobSchema = new Schema(
+  {
+    position: {
+      type: String,
+      required: true,
+      maxLength: [50, 'Position must not have more than 50 Characters'],
+    },
+    company: {
+      type: String,
+      required: [true, 'Company is a required field'],
+      maxLength: [30, 'Position must not have more than 50 Characters'],
+    },
+    role: {
+      type: String,
+      required: true,
+    },
+    level: {
+      type: String,
+      required: true,
+      enum: {
+        values: ['Junior', 'Midweight', 'Senior'],
+        message: 'Level can only include Junior, Midweight, Senior',
+      },
+    },
+    contract: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    languages: {
+      type: [String],
+      required: true,
+    },
+    tools: {
+      type: [String],
+    },
+    postedDate: {
+      type: Date,
+      required: true,
+      max: [Date.now(), 'Posted Date must not exceed current date'],
+    },
+    isNew: {
+      type: Boolean,
+      default: false,
+    },
+    isFeatured: {
+      type: Boolean,
+      default: false,
+    },
+    logo: {
+      type: String,
+      required: true,
+    },
+    // postedAt?
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Job = mongoose.model('Job', jobSchema);
+
+export default Job;
