1981ad285cb5af6d45474a36b15342a373b36c23
= <=>
2023-09-19 14:59:08 +0300

    job permission update

--- src/routes/job.js
+++ src/routes/job.js
@@ -11,12 +11,12 @@ import { isAdmin } from '../middlewares/authorize';
 
 const router = Router();
 
-router.route('/').get(getJobs).post(verifyToken, createJob);
+router.route('/').get(getJobs).post([verifyToken, isAdmin], createJob);
 
 router
   .route('/:id')
   .get(getJobById)
-  .patch(verifyToken, updateJob)
-  .delete(verifyToken, deleteJob);
+  .patch([verifyToken, isAdmin], updateJob)
+  .delete([verifyToken, isAdmin], deleteJob);
 
 export default router;
