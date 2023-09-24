a313505c32f76d8fed93682a783a05cbb174cc38
= <=>
2023-09-24 19:48:39 +0300

    users api refactored

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -2,6 +2,21 @@ import asyncHandler from 'express-async-handler';
 import User from '../models/user';
 import generateUrl from '../utils/generate-url';
 
+const generateUserResponse = (user) => {
+  return {
+    _id: user._id,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+    role: user.role,
+    isActive: user.isActive,
+    createdAt: user.createdAt,
+    updatedAt: user.updatedAt,
+    createdBy: user.createdBy,
+    updatedBy: user.updatedBy,
+  };
+};
+
 const getUserById = asyncHandler(async (req, res) => {
   const { id } = req.params;
 
@@ -12,7 +27,7 @@ const getUserById = asyncHandler(async (req, res) => {
     throw new Error('User not Found');
   }
 
-  res.json(user);
+  res.status(200).json(user);
 });
 
 const getUsers = asyncHandler(async (req, res) => {
@@ -21,6 +36,7 @@ const getUsers = asyncHandler(async (req, res) => {
   const total = await User.countDocuments(filterQueries);
 
   if (page > Math.ceil(total / limit) && total > 0) {
+    res.status(404);
     throw new Error('Page not Found');
   }
 
@@ -50,48 +66,62 @@ const getUsers = asyncHandler(async (req, res) => {
 });
 
 const createUser = asyncHandler(async (req, res) => {
+  req.body.createdBy = req.user._id;
+
   const user = await User.create(req.body);
 
-  res.status(201).json({
-    _id: user._id,
-    firstName: user.firstName,
-    lastName: user.lastName,
-    email: user.email,
-    role: user.role,
-    createdAt: user.createdAt,
-    updatedAt: user.updatedAt,
-  });
+  res.status(201).json(generateUserResponse(user));
 });
 
 const updateUser = asyncHandler(async (req, res) => {
   const { id } = req.params;
+  const { _id: userId } = req.user;
 
-  const options = {
-    new: true,
-    runValidators: true,
-  };
+  const { password } = req.body;
+  req.body.updatedBy = userId;
 
-  const updatedUser = await User.findByIdAndUpdate(id, req.body, options);
-
-  if (!updatedUser) {
-    res.status(404);
-    throw new Error('User not Found');
-  }
-
-  res.status(200).json(updatedUser);
-});
-
-const deleteUser = asyncHandler(async (req, res) => {
-  const { id } = req.params;
-
-  const user = await User.findByIdAndDelete(id);
+  const user = await User.findById(id);
 
   if (!user) {
     res.status(404);
     throw new Error('User not Found');
   }
 
-  res.status(204).json({ message: 'User Deleted' });
+  if (userId.toString() !== user.createdBy.toString()) {
+    res.status(403);
+    throw new Error('Not Authorized to update this user');
+  }
+
+  if (password) {
+    req.body.password = await user.updatePassword(password);
+  }
+
+  const updatedUser = await User.findByIdAndUpdate(id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json(generateUserResponse(updatedUser));
+});
+
+const deleteUser = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const { _id: userId } = req.user;
+  req.body.updatedBy = userId;
+
+  if (userId.toString() === id.toString()) {
+    res.status(400);
+    throw new Error('You cannot delete your own account');
+  }
+
+  const user = await User.findByIdAndUpdate(id, { isActive: false });
+
+  if (!user) {
+    res.status(404);
+    throw new Error('User not Found');
+  }
+
+  res.status(200).json({ message: 'User Deleted' });
 });
 
 export { getUserById, getUsers, createUser, updateUser, deleteUser };
--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose';
 import validator from 'validator';
-import { Schema } from 'mongoose';
 import bcrypt from 'bcryptjs';
+
+import { Schema } from 'mongoose';
 
 const userSchema = new Schema(
   {
@@ -18,21 +19,34 @@ const userSchema = new Schema(
       required: true,
       unique: true,
       lowercase: true,
-      immutable: [true, 'Email cannot be changed'],
+      immutable: true,
       validate: [validator.isEmail, 'Please enter a valid email'],
     },
     password: {
       type: String,
       required: [true, 'Please enter a password'],
-      minlength: 8,
+      minlength: [8, 'Password must be at least 8 characters long'],
     },
     role: {
       type: String,
       required: true,
       enum: {
         values: ['Candidate', 'Admin', 'Super Admin'],
-        message: 'Role can only include Candidate, Admin, Super Admin',
+        message: 'Role can only be Candidate, Admin, or Super Admin',
       },
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      immutable: true,
+    },
+    updatedBy: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
     },
   },
   { timestamps: true }
@@ -49,6 +63,11 @@ userSchema.pre('save', async function (next) {
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.methods.updatePassword = async function (newPassword) {
+  const salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(newPassword, salt);
 };
 
 const User = mongoose.model('User', userSchema);
