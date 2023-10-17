747c38a6d9e11afb87fa56e1a7059cb7299db00f
= <=>
2023-10-17 21:23:32 +0300

    http headers added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from "cors";
 import helmet from 'helmet';
 import dotenv from 'dotenv/config';
 import connectDB from './config/db';
@@ -12,6 +13,7 @@ connectDB();
 
 const app = express();
 
+app.use(cors());
 app.use(helmet());
 
 app.use(express.json());
